3b794a15e1cdac74d6abb43e3d5fdea8504465b3
9dev <9devmail@gmail.com>
2016-01-28 15:23:46 +0100

    Added caching to demo views.

--- demo/main/urls.py
+++ demo/main/urls.py
@@ -1,10 +1,26 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 from main import views
 
 
+CACHE_TIME = 60
+
+
 urlpatterns = [
-    url(r'^top$', views.TopItemListView.as_view(), name='top_item_list'),
-    url(r'^trending$', views.TrendingItemListView.as_view(), name='trending_item_list'),
-    url(r'^(?P<slug>.*)', views.ItemDetailView.as_view(), name='item_detail'),
+    url(
+        r'^top$',
+        cache_page(CACHE_TIME)(views.TopItemListView.as_view()),
+        name='top_item_list'
+    ),
+    url(
+        r'^trending$',
+        cache_page(CACHE_TIME)(views.TrendingItemListView.as_view()),
+        name='trending_item_list'
+    ),
+    url(
+        r'^(?P<slug>.*)',
+        cache_page(CACHE_TIME)(views.ItemDetailView.as_view()),
+        name='item_detail'
+    ),
 ]
